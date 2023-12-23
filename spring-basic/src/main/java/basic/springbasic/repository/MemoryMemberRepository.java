ca71149172f4d5cd9860d2b79a574279f2421301
insoo00 <insoosos1234@gmail.com>
2023-12-23 22:55:05 +0900

    spring bean by component scan and dependency injection

--- spring-basic/src/main/java/basic/springbasic/repository/MemoryMemberRepository.java
+++ spring-basic/src/main/java/basic/springbasic/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package basic.springbasic.repository;
 
 import basic.springbasic.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
--- spring-basic/src/main/java/basic/springbasic/service/MemberService.java
+++ spring-basic/src/main/java/basic/springbasic/service/MemberService.java
@@ -3,14 +3,18 @@ package basic.springbasic.service;
 import basic.springbasic.domain.Member;
 import basic.springbasic.repository.MemberRepository;
 import basic.springbasic.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
