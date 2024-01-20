df48aada565459cb86d7131b069510fc04ee2fd7
insoo00 <insoosos1234@gmail.com>
2024-01-20 17:41:12 +0900

    read member

--- spring-basic/src/main/java/basic/springbasic/controller/HomeController.java
+++ spring-basic/src/main/java/basic/springbasic/controller/HomeController.java
@@ -4,8 +4,11 @@ import basic.springbasic.domain.Member;
 import basic.springbasic.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -37,4 +40,10 @@ public class HomeController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
