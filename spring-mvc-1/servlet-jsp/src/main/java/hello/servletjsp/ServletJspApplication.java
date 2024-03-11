d8201124516f2e8e1091f5b7db8f2cc7d5b8485f
insoo00 <insoosos1234@gmail.com>
2024-03-11 23:28:58 +0900

    :tada: Initial commit

---
+++ spring-mvc-1/servlet-jsp/src/main/java/hello/servletjsp/ServletJspApplication.java
@@ -0,0 +1,13 @@
+package hello.servletjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletJspApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-1/servlet-jsp/src/test/java/hello/servletjsp/ServletJspApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servletjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
