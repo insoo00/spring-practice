e7029af97a846064aedec716dc036c162b3a55ec
insoo00 <insoosos1234@gmail.com>
2024-03-15 18:59:24 +0900

    :sparkles: Add frontController (v2)

---
+++ spring-mvc-1/servlet-jsp/src/main/java/hello/servletjsp/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package hello.servletjsp.web.frontcontroller.v2.controller;
+
+import hello.servletjsp.domain.member.Member;
+import hello.servletjsp.domain.member.MemberRepository;
+import hello.servletjsp.web.frontcontroller.MyView;
+import hello.servletjsp.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
