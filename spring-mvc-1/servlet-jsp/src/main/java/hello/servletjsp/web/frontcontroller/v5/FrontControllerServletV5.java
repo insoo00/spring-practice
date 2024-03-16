e0dd83fd9b1886ce348dcc437a9fb960ec5b68e6
insoo00 <insoosos1234@gmail.com>
2024-03-17 00:54:11 +0900

    :sparkles: Add frontController (v5-v3)

---
+++ spring-mvc-1/servlet-jsp/src/main/java/hello/servletjsp/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,77 @@
+package hello.servletjsp.web.frontcontroller.v5;
+
+import hello.servletjsp.web.frontcontroller.ModelView;
+import hello.servletjsp.web.frontcontroller.MyView;
+import hello.servletjsp.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servletjsp.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servletjsp.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servletjsp.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMapping = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMapping();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerMapping() {
+        handlerMapping.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMapping.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMapping.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    private boolean initHandlerAdapters() {
+        return handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Object handler = getHandler(req);
+        if (handler == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView mv = adapter.handle(req, resp, handler);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), req, resp);
+    }
+
+    private Object getHandler(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        return handlerMapping.get(requestURI);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter handlerAdapter : handlerAdapters) {
+            if (handlerAdapter.supports(handler)) {
+                return handlerAdapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler=" + handler);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
