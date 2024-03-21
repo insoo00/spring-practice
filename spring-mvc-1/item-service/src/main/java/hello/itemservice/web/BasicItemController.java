8319f01cffc29ee067df23966c99e85ee51bbb89
insoo00 <insoosos1234@gmail.com>
2024-03-21 19:23:51 +0900

    :sparkles: Add item registration

--- spring-mvc-1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
+++ spring-mvc-1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +32,54 @@ public class BasicItemController {
             Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam("itemName") String itemName,
+            @RequestParam("price") int price,
+            @RequestParam("quantity") Integer quantity,
+            Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV2(
+            @ModelAttribute("item") Item item,
+            Model model) {
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(
+            @ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+
         return "basic/item";
     }
 
