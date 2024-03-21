a44891fe17acaa65ca1260ad646f86629f6086ff
insoo00 <insoosos1234@gmail.com>
2024-03-21 17:14:24 +0900

    :sparkles: Add item detail

--- spring-mvc-1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
+++ spring-mvc-1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +26,15 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(
+            @PathVariable("itemId") long itemId,
+            Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     @PostConstruct
