f4a1b053b5951ffeac7064e91eaf65cd06cec4ad
insoo00 <insoosos1234@gmail.com>
2024-04-05 16:57:24 +0900

    :tada: Initial commit

---
+++ spring-mvc-2/thymeleafForm/src/test/java/hello/thymeleafForm/ThymeleafFormApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleafForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
