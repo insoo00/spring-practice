9f74c48b9f859636bd2fd7b829ac2086f0fc12da
insoo00 <insoosos1234@gmail.com>
2024-04-06 23:43:37 +0900

    :sparkles: Apply bean validation in spring

--- spring-mvc-2/validation/src/main/java/hello/itemservice/validation/web/validation/ValidationItemControllerV3.java
+++ spring-mvc-2/validation/src/main/java/hello/itemservice/validation/web/validation/ValidationItemControllerV3.java
@@ -21,13 +21,6 @@ import java.util.List;
 public class ValidationItemControllerV3 {
 
     private final ItemRepository itemRepository;
-    private final ItemValidator itemValidator;
-
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
-        log.info("init binder {}", dataBinder);
-        dataBinder.addValidators(itemValidator);
-    }
 
     @GetMapping
     public String items(Model model) {
@@ -50,7 +43,7 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/add")
-    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //검증 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
