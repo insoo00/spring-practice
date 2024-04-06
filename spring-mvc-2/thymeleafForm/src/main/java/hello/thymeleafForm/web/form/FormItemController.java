47918c2624e36485a06e412741738082c0b2009e
insoo00 <insoosos1234@gmail.com>
2024-04-06 14:35:03 +0900

    :sparkles: Add single checkbox

--- spring-mvc-2/thymeleafForm/src/main/java/hello/thymeleafForm/web/form/FormItemController.java
+++ spring-mvc-2/thymeleafForm/src/main/java/hello/thymeleafForm/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.thymeleafForm.web.form;
 import hello.thymeleafForm.domain.item.Item;
 import hello.thymeleafForm.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -35,6 +37,17 @@ public class FormItemController {
     public String addItem(Model model) {
         model.addAttribute("item", new Item());
         return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/form/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
