a862a111735a68800e84708c4609ab9782493908
insoo00 <insoosos1234@gmail.com>
2024-04-25 10:04:33 +0900

    :sparkles: Apply @Login

--- spring-mvc-2/login/src/main/java/hello/login/web/HomeController.java
+++ spring-mvc-2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,13 +2,13 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -20,7 +20,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String homeLogin(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            @Login Member loginMember,
             Model model) {
         if (loginMember == null) {
             return "home";
