c7a5d0920ad0cb6456e99ceebb838a5bafed7f7a
insoo00 <insoosos1234@gmail.com>
2024-05-02 23:11:51 +0900

    :tada: initial commit

---
+++ oauth2Session/src/test/java/spring/security/oauth2Session/Oauth2SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.oauth2Session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
