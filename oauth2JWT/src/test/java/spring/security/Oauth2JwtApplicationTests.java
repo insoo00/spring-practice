968239a7c55db7c7818a02d85c3e284980d8b99b
insoo00 <insoosos1234@gmail.com>
2024-05-06 18:54:47 +0900

    :tada: initial commit

---
+++ oauth2JWT/src/test/java/spring/security/Oauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
